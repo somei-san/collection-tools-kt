53c5b999395fcc2b283804b4ddeb118609efa934
somei <10me1goya@gmail.com>
2023-05-30 19:40:37 +0900

    🗑️ 不要な記述を削除

--- src/main/kotlin/Sorting.kt
+++ src/main/kotlin/Sorting.kt
@@ -1,4 +1,3 @@
-
 /** [this] がソートされていたら true を返します */
-inline fun <reified T : Comparable<T>> List<T>.isSorted(): Boolean =
+fun <T : Comparable<T>> List<T>.isSorted(): Boolean =
     this.zipWithNext { a, b -> a.compareTo(b) }.all { it <= 0 }
